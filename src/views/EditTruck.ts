9d24146e2da00a0355c08aaba3e868fc9454b16a
mihail.stefanov <mihail.stefanov@amusnet.com>
2022-12-05 22:17:46 +0200

    changed button text depending on type

--- src/views/EditTruck.ts
+++ src/views/EditTruck.ts
@@ -1,9 +1,10 @@
 import { span, input, label, select, option, button, form, div } from "../dom/dom";
 import { getEnum } from "../trucks";
+import { getLocation } from "../utils";
 
 export function EditTruck(keys) {
     const enums = getEnum();
-    
+
     const fields = keys.map(key => {
         for (let i = 0; i < enums.length; i++) {
             let en = enums[i];
@@ -22,7 +23,9 @@ export function EditTruck(keys) {
         const currentInput = input({ type: "text", name: key });
         return label({}, currentSpan, currentInput);
     });
-    const editBtn = button({ className: "action confirm", type: "submit", id: "edit" }, "Save Car");
+    const type = getLocation().slice(0, -1);
+    const capitalizedType = type[0].toLocaleUpperCase() + type.slice(1);
+    const editBtn = button({ className: "action confirm", type: "submit", id: "edit" }, `Save ${capitalizedType}`);
     const cancelBtn = button({ className: "action cancel", type: "reset" }, "Cancel");
     const buttonWrapperDiv = div({}, editBtn, cancelBtn);
     return form({ className: "align", id: "edit" }, ...fields, buttonWrapperDiv)
