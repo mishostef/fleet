4690f0de8b9f7dd3475e33c24ac4b020e5ea18e3
mihail.stefanov <mihail.stefanov@amusnet.com>
2022-12-09 10:12:18 +0200

    refaactoring, added price validator

--- src/models/validators.ts
+++ src/models/validators.ts
@@ -1,15 +1,25 @@
 import { getLocation } from "../utils";
+const makeValidator = (make) => { if (make.trim() === "") throw new RangeError("make cannot be empty") }
+const modelValidator = (model) => { if (model.trim() === "") throw new RangeError("model cannot be empty") }
+const numberOfSeatsValidator = (numberOfSeats) => { if (isNaN(numberOfSeats) || Number(numberOfSeats) < 1 || Number(numberOfSeats) > 9) throw new RangeError("1 < numberOfSeats< 9") }
+const capacityValidator = (capacity) => { if (isNaN(capacity) || Number(capacity) < 1 || Number(capacity) > 4) throw new RangeError("1 < Capacity <= 4 ") }
 
+const rentValidator = (rentalPrice) => {
+    if (isNaN(rentalPrice) || Number(rentalPrice) <= 0
+    ) { throw new Error('price should be a positive number'); }
+}
 const carValidator = {
-    "make": (make) => { if (make.trim() === "") throw new RangeError("make cannot be empty") },
-    "model": (model) => { if (model.trim() === "") throw new RangeError("model cannot be empty") },
-    "numberOfSeats": (numberOfSeats) => { if (isNaN(numberOfSeats) || +numberOfSeats < 1 || +numberOfSeats > 9) throw new RangeError("1 < numberOfSeats< 9") },
+    "make": makeValidator,
+    "model": modelValidator,
+    "numberOfSeats": numberOfSeatsValidator,
+    "rentalPrice": rentValidator
 };
 
 const truckValidator = {
-    "make": (make) => { if (make.trim() === "") throw new RangeError("make cannot be empty") },
-    "model": (model) => { if (model.trim() === "") throw new RangeError("model cannot be empty") },
-    "capacity": (capacity) => { if (isNaN(capacity) || +capacity < 1 || +capacity > 4) throw new RangeError("1 < Capacity <= 4 ") },
+    "make": makeValidator,
+    "model": modelValidator,
+    "capacity": capacityValidator,
+    "rentalPrice": rentValidator
 };
 
 
--- src/views/EditVehicle.ts
+++ src/views/EditVehicle.ts
@@ -11,14 +11,12 @@ export function EditTruck(keys) {
             const enumKey = Object.keys(en)[0];
             const enumVals = Object.values(en[enumKey]).filter(v => isNaN(Number(v)));
             if (key === enumKey) {
-                const values = enumVals;
-                const options = values.map(val => option({ value: val, textContent: val }));
+                const options = enumVals.map(val => option({ value: val, textContent: val }));
                 const currentSpan = span({}, key);
                 const currentSelect = select({ name: key }, ...options);
                 return label({}, currentSpan, currentSelect);
             }
         }
-
         const currentSpan = span({}, key.replace(/([a-z])([A-Z])/g, "$1 $2").toLowerCase());
         const currentInput = input({ type: "text", name: key });
         return label({}, currentSpan, currentInput);
